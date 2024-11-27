734d4ec75c13519e9a80e54337ddb2db7b240849
Jhung Minwoo <minwoorich96@gmail.com>
2024-11-27 23:28:56 +0900

    init commit

---
+++ src/main/java/co/devsco/everyteen_be/EveryteenBeApplication.java
@@ -0,0 +1,13 @@
+package co.devsco.everyteen_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryteenBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryteenBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/devsco/everyteen_be/EveryteenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.devsco.everyteen_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryteenBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
