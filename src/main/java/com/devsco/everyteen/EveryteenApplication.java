5df935f16ba41d517004491e1f43ef868cca166a
Jhung Minwoo <minwoorich96@gmail.com>
2024-12-06 21:27:08 +0900

    ✨ pre-commit 깃 훅 등록 테스트

---
+++ src/main/java/com/devsco/everyteen/EveryteenApplication.java
@@ -0,0 +1,11 @@
+package com.devsco.everyteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryteenApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EveryteenApplication.class, args);
+  }
+}
