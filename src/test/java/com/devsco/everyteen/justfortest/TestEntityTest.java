9501f1390ddcc5adb4cd0c72d0aabe4facf3c57e
Jhung Minwoo <minwoorich96@gmail.com>
2024-12-07 12:39:00 +0900

    🧪 H2 DB 연결 테스트 코드 작성 및 수행

--- src/test/java/com/devsco/everyteen/justfortest/TestEntityTest.java
+++ src/test/java/com/devsco/everyteen/justfortest/TestEntityTest.java
@@ -1,21 +1,22 @@
 package com.devsco.everyteen.justfortest;
 
+import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @DataJpaTest
-@TestPropertySource(locations = "classpath:application-test.yml")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class TestEntityTest {
 
   @DisplayName("H2 DB 동작 테스트")
   @Test
-  void test(@Autowired TestEntityRepository testEntityRepository) {
+  void test(@Autowired TestEntityRepository testEntityRepository, @Autowired EntityManager em) {
     //given
     TestEntity test1 = TestEntity.builder().name("홍길동").build();
 
@@ -23,6 +24,6 @@ public class TestEntityTest {
     TestEntity saved1 = testEntityRepository.save(test1);
 
     //then
-    Assertions.assertThat(saved1).isEqualTo(test1);
+    Assertions.assertThat(saved1.getName()).isEqualTo("홍길동");
   }
 }
