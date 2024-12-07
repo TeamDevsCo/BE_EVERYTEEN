f75227d4e877eaa143a94d4c29407370189369c5
Minwoo Jhung <minwoorich@naver.com>
2024-12-07 20:39:03 +0900

    ✨ 공통 예외 처리기 + 공통 예외 응답 포맷 작성 완료

---
+++ src/main/java/com/devsco/everyteen/exception/CustomErrorResponse.java
@@ -0,0 +1,62 @@
+package com.devsco.everyteen.exception;
+
+import lombok.Builder;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+public record CustomErrorResponse(
+  String title,
+  String detail,
+  int status,
+  URI uri,
+  List<FieldErrorInfo> invalids
+) {
+  public CustomErrorResponse(ProblemDetail problem) {
+    this(problem.getTitle(), problem.getDetail(), problem.getStatus(), problem.getInstance(), new ArrayList<>());
+  }
+
+  public static CustomErrorResponse from(MethodArgumentNotValidException ex) {
+    CustomErrorResponse errorResponse = new CustomErrorResponse(ex.getBody());
+    errorResponse.invalids.addAll(FieldErrorInfo.from(ex.getAllErrors()));
+    return new CustomErrorResponse(ex.getBody());
+  }
+
+  public static CustomErrorResponse from(HandlerMethodValidationException ex) {
+    CustomErrorResponse errorResponse = new CustomErrorResponse(ex.getBody());
+    errorResponse.invalids.addAll(FieldErrorInfo.from(ex.getAllErrors()));
+    return new CustomErrorResponse(ex.getBody());
+  }
+
+  @Builder
+  public record FieldErrorInfo(
+    String field,
+    String message,
+    Object[] arguments,
+    String rejectValue,
+    String[] codes
+  ) {
+
+    public static List<FieldErrorInfo> from(List<? extends MessageSourceResolvable> errors) {
+      return errors.stream().map(FieldErrorInfo::from).toList();
+    }
+
+    private static FieldErrorInfo from(MessageSourceResolvable error) {
+      var builder = FieldErrorInfo.builder();
+      if (error instanceof FieldError fieldError) {
+        builder.field(fieldError.getField());
+      }
+      return builder
+        .codes(error.getCodes())
+        .message(error.getDefaultMessage())
+        .arguments(error.getArguments())
+        .build();
+    }
+  }
+}
--- src/main/java/com/devsco/everyteen/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/devsco/everyteen/exception/ExceptionHandlerAdvice.java
@@ -1,22 +1,36 @@
 package com.devsco.everyteen.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.ErrorResponse;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 @Slf4j
 public class ExceptionHandlerAdvice {
 
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(BAD_REQUEST)
+  public CustomErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    return CustomErrorResponse.from(ex);
+  }
+
+  @ExceptionHandler(HandlerMethodValidationException.class)
+  @ResponseStatus(BAD_REQUEST)
+  public CustomErrorResponse handleMethodArgumentNotValidException(HandlerMethodValidationException ex) {
+    return CustomErrorResponse.from(ex);
+  }
+
   @ExceptionHandler(Exception.class)
   @ResponseStatus(INTERNAL_SERVER_ERROR)
-  public ErrorResponse handleRestException(Exception ex) {
-    // TODO: Implement exception handling
+  public CustomErrorResponse handleRestException(Exception ex) {
     log.error(INTERNAL_SERVER_ERROR.getReasonPhrase(), ex);
-    return null;
+    return new CustomErrorResponse(ProblemDetail.forStatus(INTERNAL_SERVER_ERROR));
   }
 }
